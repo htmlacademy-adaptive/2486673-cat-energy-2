ffc6985ccf390da50696d973a4a1a730e2d6b76a
EgorA41 <egor8619@mail.ru>
2024-09-03 13:26:03 +0700

    1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,19 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-navigation--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-navigation--is-close')) {
+    navMain.classList.remove('main-navigation--is-close');
+    navMain.classList.add('main-navigation--is-open');
+    navToggle.classList.remove ('main-header__toggle--open');
+    navToggle.classList.add ('main-header__toggle--close');
+  } else {
+    navMain.classList.add('main-navigation--is-close');
+    navMain.classList.remove('main-navigation--is-open');
+    navToggle.classList.add ('main-header__toggle--open');
+    navToggle.classList.remove ('main-header__toggle--close');
+  }
+});
+
